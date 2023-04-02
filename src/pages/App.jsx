c8b7ffa947ba71deddb74db41c5ca6cefc936f4b
MShafiMS <shahedali734@gmail.com>
2023-04-03 05:17:18 +0600

    made logo succesfully

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Components/pages/Home/Home";
+import Nav from "./Components/shared/Nav";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Components/shared/Nav.jsx
@@ -0,0 +1,14 @@
+import Logo from "../images/Logo";
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <Logo />
+        <h1>Logo</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
