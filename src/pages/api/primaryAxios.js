c1213b18988c17e04f186d7edfbcb3b080376ddc
MShafiMS <shahedali734@gmail.com>
2023-05-01 23:50:42 +0600

    updated api

--- src/pages/api/primaryAxios.js
+++ src/pages/api/primaryAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const primaryAxios = axios.create({
-  baseURL: "https://motion-diary.onrender.com/",
+  baseURL: "https://motion-diary-server.vercel.app/",
 });
 
 export default primaryAxios;
