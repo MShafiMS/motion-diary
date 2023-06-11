7b391225fdcef5be78035214b6e58cc23e470b59
MShafiMS <shahedali734@gmail.com>
2023-06-11 18:31:05 +0600

    updated

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 //Midddle War
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nrymvre.mongodb.net/?retryWrites=true&w=majority`;
 
