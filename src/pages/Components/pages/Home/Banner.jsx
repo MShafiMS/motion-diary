ca850e23f46da45ab33f6420658d7b3bd37eebfa
MShafiMS <shahedali734@gmail.com>
2023-06-11 18:55:16 +0600

    updated

--- src/pages/Components/pages/Home/Banner.jsx
+++ src/pages/Components/pages/Home/Banner.jsx
@@ -34,58 +34,62 @@ const Banner = () => {
         modules={[Navigation, Pagination]}
         className="mySwiper"
       >
-        {blogs?.data?.slice(0, 5).map((blog) => (
-          <div key={blog?._id}>
-            {blog?.approve && (
-              <SwiperSlide key={blog?._id}>
-                <div
-                  className="h-[60vh] relative w-full"
-                  style={{
-                    backgroundImage: `url(${blog?.img})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                  }}
-                >
-                  {/* overlay */}
-                  <div className="w-full h-full bg-gradient-to-t from-black/70 absolute top-0 left-0" />
-                  {/* navigation */}
-                  <button
-                    className={`lg:block hidden absolute left-6 custom-swiper-button-prev text-white border-2 hover:bg-primary hover:border-primary duration-300 z-20 p-3 rounded-full top-1/2 -translate-y-1/2 ${
-                      isHover ? "opacity-1000" : "opacity-0"
-                    }`}
+        {blogs?.data
+          .slice()
+          .reverse()
+          .slice(0, 5)
+          .map((blog) => (
+            <div key={blog?._id}>
+              {blog?.approve && (
+                <SwiperSlide key={blog?._id}>
+                  <div
+                    className="h-[60vh] relative w-full"
+                    style={{
+                      backgroundImage: `url(${blog?.img})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                    }}
                   >
-                    <FaLongArrowAltLeft />
-                  </button>
-                  <button
-                    className={`lg:block hidden absolute right-6 custom-swiper-button-next text-white border-2 hover:bg-primary hover:border-primary duration-300 z-20 p-3 rounded-full top-1/2 -translate-y-1/2 ${
-                      isHover ? "opacity-1000" : "opacity-0"
-                    }`}
-                  >
-                    <FaLongArrowAltRight />
-                  </button>
-                  <div className="text-white h-full w-full absolute z-10 top-0 left-0 flex flex-col gap-6 items-center justify-center">
-                    <button className="text-xs font-medium uppercase bg-primary px-2 py-1">
-                      {blog?.category}
+                    {/* overlay */}
+                    <div className="w-full h-full bg-gradient-to-t from-black/70 absolute top-0 left-0" />
+                    {/* navigation */}
+                    <button
+                      className={`lg:block hidden absolute left-6 custom-swiper-button-prev text-white border-2 hover:bg-primary hover:border-primary duration-300 z-20 p-3 rounded-full top-1/2 -translate-y-1/2 ${
+                        isHover ? "opacity-1000" : "opacity-0"
+                      }`}
+                    >
+                      <FaLongArrowAltLeft />
                     </button>
-                    <a
-                      href="#"
-                      className="lg:text-4xl text-xl max-w-lg duration-300 font-bold hover:underline"
+                    <button
+                      className={`lg:block hidden absolute right-6 custom-swiper-button-next text-white border-2 hover:bg-primary hover:border-primary duration-300 z-20 p-3 rounded-full top-1/2 -translate-y-1/2 ${
+                        isHover ? "opacity-1000" : "opacity-0"
+                      }`}
                     >
-                      {blog?.title}
-                    </a>
-                    <p className="uppercase text-xs italic">{blog?.date}</p>
-                    <Link
-                      href={`/blog/${blog?._id}`}
-                      className="border-white border px-5 hover:bg-primary hover:border-primary duration-500 py-3 text-xs uppercase"
-                    >
-                      Continue Reading
-                    </Link>
+                      <FaLongArrowAltRight />
+                    </button>
+                    <div className="text-white h-full w-full absolute z-10 top-0 left-0 flex flex-col gap-6 items-center justify-center">
+                      <button className="text-xs font-medium uppercase bg-primary px-2 py-1">
+                        {blog?.category}
+                      </button>
+                      <Link
+                        href={`/blog/${blog?._id}`}
+                        className="lg:text-4xl text-xl max-w-lg duration-300 font-bold hover:underline"
+                      >
+                        {blog?.title}
+                      </Link>
+                      <p className="uppercase text-xs italic">{blog?.date}</p>
+                      <Link
+                        href={`/blog/${blog?._id}`}
+                        className="border-white border px-5 hover:bg-primary hover:border-primary duration-500 py-3 text-xs uppercase"
+                      >
+                        Continue Reading
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            )}
-          </div>
-        ))}
+                </SwiperSlide>
+              )}
+            </div>
+          ))}
       </Swiper>
     </div>
   );
